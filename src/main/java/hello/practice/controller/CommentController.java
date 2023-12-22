3de5fe57411c440ffc0488e21f550fa6e9143a0c
gyutae <qoxowkd0716@gmail.com>
2023-12-22 15:17:22 +0900

    Comment 조회 기능 구현

--- src/main/java/hello/practice/controller/CommentController.java
+++ src/main/java/hello/practice/controller/CommentController.java
@@ -4,6 +4,8 @@ import hello.practice.dto.CommentResponseDto;
 import hello.practice.dto.CreateCommentRequestDto;
 import hello.practice.service.CommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +21,10 @@ public class CommentController {
     @PostMapping
     public CommentResponseDto createComment(@RequestBody CreateCommentRequestDto requestDto) {
         return commentService.createComment(requestDto);
+    }
+
+    @GetMapping("/{id}")
+    public CommentResponseDto getCommentById(@PathVariable Long id) {
+        return commentService.getCommentById(id);
     }
 }
--- src/main/java/hello/practice/service/CommentService.java
+++ src/main/java/hello/practice/service/CommentService.java
@@ -4,6 +4,8 @@ import hello.practice.dto.CommentResponseDto;
 import hello.practice.dto.CreateCommentRequestDto;
 import hello.practice.entity.Comment;
 import hello.practice.repository.CommentRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +21,12 @@ public class CommentService {
             .content(request.content())
             .build();
         comment = commentRepository.save(comment);
+        return new CommentResponseDto(comment.getId(), comment.getPostId(), comment.getContent());
+    }
+
+    public CommentResponseDto getCommentById(Long id) {
+        Comment comment = commentRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Comment not found"));
         return new CommentResponseDto(comment.getId(), comment.getPostId(), comment.getContent());
     }
 
