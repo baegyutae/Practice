14e37ccd0d2d82ee666e4f81d0e2cced1d02c60b
gyutae <qoxowkd0716@gmail.com>
2023-12-22 12:51:18 +0900

    게시물 전체 조회

--- src/main/java/hello/practice/controller/PostController.java
+++ src/main/java/hello/practice/controller/PostController.java
@@ -3,7 +3,9 @@ package hello.practice.controller;
 import hello.practice.dto.CreatePostRequestDto;
 import hello.practice.dto.PostResponse;
 import hello.practice.service.PostService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +21,10 @@ public class PostController {
     @PostMapping
     public PostResponse createPost(@RequestBody CreatePostRequestDto requestDto) {
         return postService.createPost(requestDto);
+    }
+
+    @GetMapping
+    public List<PostResponse> getAllPosts() {
+        return postService.getAllPosts();
     }
 }
--- src/main/java/hello/practice/service/PostService.java
+++ src/main/java/hello/practice/service/PostService.java
@@ -4,6 +4,8 @@ import hello.practice.dto.CreatePostRequestDto;
 import hello.practice.dto.PostResponse;
 import hello.practice.entity.Post;
 import hello.practice.repository.PostRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,12 @@ public class PostService {
     public PostResponse createPost(CreatePostRequestDto requestDto) {
         Post post = postRepository.save(new Post(requestDto.title(), requestDto.content()));
         return new PostResponse(post.getId(), post.getTitle(), post.getContent());
+    }
+
+    public List<PostResponse> getAllPosts() {
+        return postRepository.findAll().stream()
+            .map(post -> new PostResponse(post.getId(), post.getTitle(), post.getContent()))
+            .collect(Collectors.toList());
     }
 
 }
