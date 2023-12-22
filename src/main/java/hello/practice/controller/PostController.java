fe4336a5087396762bbccffcc45d4cf857ac1dd2
gyutae <qoxowkd0716@gmail.com>
2023-12-22 12:19:27 +0900

    PostController 구현

---
+++ src/main/java/hello/practice/controller/PostController.java
@@ -0,0 +1,16 @@
+package hello.practice.controller;
+
+import hello.practice.service.PostService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+}
