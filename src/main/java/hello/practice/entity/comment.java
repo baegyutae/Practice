8c8124a0919ed840ad93a8e119beede8c64b9006
gyutae <qoxowkd0716@gmail.com>
2023-12-22 14:52:14 +0900

    Comment 엔티티

---
+++ src/main/java/hello/practice/entity/comment.java
@@ -0,0 +1,26 @@
+package hello.practice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long postId; // 게시글 ID
+
+    private String content;
+}
